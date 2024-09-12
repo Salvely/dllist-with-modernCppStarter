e8d0c568d8aaf31854f1a67b179fe77e58737882
Salvely <iamgwen9@gmail.com>
2024-09-12 18:49:26 +0800

    build unit testing skeleton

---
+++ test/source/dllist_test.cpp
@@ -0,0 +1,29 @@
+#include "static/dllist.hpp"
+
+#include <doctest/doctest.h>
+
+TEST_CASE("Initialization and destruction test") { Dllist dllist; }
+
+TEST_CASE("insert front test") { Dllist dllist; }
+
+TEST_CASE("insert back test") { Dllist dllist; }
+
+TEST_CASE("insert item test") { Dllist dllist; }
+
+TEST_CASE("delete front test") { Dllist dllist; }
+
+TEST_CASE("delete back test") { Dllist dllist; }
+
+TEST_CASE("delete item test") { Dllist dllist; }
+
+TEST_CASE("find test") { Dllist dllist; }
+
+TEST_CASE("modify test") { Dllist dllist; }
+
+TEST_CASE("empty test") { Dllist dllist; }
+
+TEST_CASE("size test") { Dllist dllist; }
+
+TEST_CASE("print list test") { Dllist dllist; }
+
+TEST_CASE("integration test") { Dllist dllist; }
