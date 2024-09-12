3441f249c72f1eb2e355921b32fe0c7e6b2e1d7c
Salvely <iamgwen9@gmail.com>
2024-09-12 18:55:47 +0800

    format the dllist test

--- test/source/dllist_test.cpp
+++ test/source/dllist_test.cpp
@@ -2,54 +2,28 @@
 
 #include <doctest/doctest.h>
 
-TEST_CASE("Initialization and destruction test") {
-    dllist<int> dllist;
-}
+TEST_CASE("Initialization and destruction test") { dllist<int> dllist; }
 
-TEST_CASE("insert front test") {
-    dllist<int> dllist;
-}
+TEST_CASE("insert front test") { dllist<int> dllist; }
 
-TEST_CASE("insert back test") {
-    dllist<int> dllist;
-}
+TEST_CASE("insert back test") { dllist<int> dllist; }
 
-TEST_CASE("insert item test") {
-    dllist<int> dllist;
-}
+TEST_CASE("insert item test") { dllist<int> dllist; }
 
-TEST_CASE("delete front test") {
-    dllist<int> dllist;
-}
+TEST_CASE("delete front test") { dllist<int> dllist; }
 
-TEST_CASE("delete back test") {
-    dllist<int> dllist;
-}
+TEST_CASE("delete back test") { dllist<int> dllist; }
 
-TEST_CASE("delete item test") {
-    dllist<int> dllist;
-}
+TEST_CASE("delete item test") { dllist<int> dllist; }
 
-TEST_CASE("find test") {
-    dllist<int> dllist;
-}
+TEST_CASE("find test") { dllist<int> dllist; }
 
-TEST_CASE("modify test") {
-    dllist<int> dllist;
-}
+TEST_CASE("modify test") { dllist<int> dllist; }
 
-TEST_CASE("empty test") {
-    dllist<int> dllist;
-}
+TEST_CASE("empty test") { dllist<int> dllist; }
 
-TEST_CASE("size test") {
-    dllist<int> dllist;
-}
+TEST_CASE("size test") { dllist<int> dllist; }
 
-TEST_CASE("print list test") {
-    dllist<int> dllist;
-}
+TEST_CASE("print list test") { dllist<int> dllist; }
 
-TEST_CASE("integration test") {
-    dllist<int> dllist;
-}
+TEST_CASE("integration test") { dllist<int> dllist; }
