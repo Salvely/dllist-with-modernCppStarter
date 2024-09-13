a4d41f397839322c69b7b58a9b7f3e6f6cb89a4c
Salvely <iamgwen9@gmail.com>
2024-09-13 15:54:41 +0800

    remove SUBCASE

--- test/source/dllist_test.cpp
+++ test/source/dllist_test.cpp
@@ -9,45 +9,45 @@ TEST_CASE("Initialization test") {
   // assert that the head is nullptr
   REQUIRE(dllist.get_head() == nullptr);
   // assert that the size = 0
-  SUBCASE("size test") { REQUIRE(dllist.get_size() == 0); }
+  REQUIRE(dllist.get_size() == 0);
   // assert that empty() returns true
-  SUBCASE("empty test") { REQUIRE(dllist.empty() == true); }
+  REQUIRE(dllist.empty() == true);
   // print list
   INFO("The dllist: ");
   dllist.print_list();
 }
 
-TEST_CASE("insert front and delete front test") {
+TEST_SUITE("insert front and delete front test") {
   dllist<int> dllist;
 
-  SUBCASE("test insert front") {
-    INFO("insert 5 numbers to the list at the front");
-    for (int i = 0; i < 5; i++) {
-      dllist.insert_front(i);
-    }
-    INFO("assert that the size = 5");
-    REQUIRE(dllist.get_size() == 5);
-    INFO("assert that empty() returns false");
-    REQUIRE(dllist.empty() == false);
+  // SUBCASE("test insert front") {
+  INFO("insert 5 numbers to the list at the front");
+  for (int i = 0; i < 5; i++) {
+    dllist.insert_front(i);
   }
+  INFO("assert that the size = 5");
+  REQUIRE(dllist.get_size() == 5);
+  INFO("assert that empty() returns false");
+  REQUIRE(dllist.empty() == false);
+  // }
 
   INFO("dllist: ");
   dllist.print_list();
 
-  SUBCASE("test delete front") {
-    REQUIRE(dllist.empty() == false);
-    INFO("delete from the front");
-    for (int i = 0; i < 5; i++) {
-      int val = dllist.delete_front();
-      INFO("deleted ", val);
-    }
-    INFO("assert that the size = 0");
-    REQUIRE(dllist.get_size() == 0);
-    INFO("assert that empty() returns true");
-    REQUIRE(dllist.empty() == true);
-    INFO("dllist: ");
-    dllist.print_list();
+  // SUBCASE("test delete front") {
+  REQUIRE(dllist.empty() == false);
+  INFO("delete from the front");
+  for (int i = 0; i < 5; i++) {
+    int val = dllist.delete_front();
+    INFO("deleted ", val);
   }
+  INFO("assert that the size = 0");
+  REQUIRE(dllist.get_size() == 0);
+  INFO("assert that empty() returns true");
+  REQUIRE(dllist.empty() == true);
+  INFO("dllist: ");
+  dllist.print_list();
+  // }
 }
 
 TEST_CASE("insert back and delete back test") {
