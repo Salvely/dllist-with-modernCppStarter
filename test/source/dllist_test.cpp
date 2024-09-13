21d580109ebae8b89a89e2e7c3bc1abd5f263a52
Salvely <iamgwen9@gmail.com>
2024-09-13 14:44:11 +0800

    fix delete_front, set next and prev to null after change the pointer

--- test/source/dllist_test.cpp
+++ test/source/dllist_test.cpp
@@ -4,66 +4,75 @@
 #include <stdbool.h>
 
 TEST_CASE("Initialization test") {
-  dllist<int> dllist;
-  INFO("initiliaze a new dllist");
-  // assert that the head is nullptr
-  REQUIRE(dllist.get_head() == nullptr);
-  // assert that the size = 0
-  SUBCASE("size test") { REQUIRE(dllist.get_size() == 0); }
-  // assert that empty() returns true
-  SUBCASE("empty test") { REQUIRE(dllist.empty() == true); }
-  // print list
-  INFO("The dllist: ");
-  dllist.print_list();
+    dllist<int> dllist;
+    INFO("initiliaze a new dllist");
+    // assert that the head is nullptr
+    REQUIRE(dllist.get_head() == nullptr);
+    // assert that the size = 0
+    SUBCASE("size test") {
+        REQUIRE(dllist.get_size() == 0);
+    }
+    // assert that empty() returns true
+    SUBCASE("empty test") {
+        REQUIRE(dllist.empty() == true);
+    }
+    // print list
+    INFO("The dllist: ");
+    dllist.print_list();
 }
 
 TEST_CASE("insert front and delete front test") {
-  dllist<int> dllist;
+    dllist<int> dllist;
 
-  INFO("insert 5 numbers to the list at the front");
-  for (int i = 0; i < 5; i++) {
-    dllist.insert_front(i);
-  }
-  INFO("assert that the size = 5");
-  REQUIRE(dllist.get_size() == 5);
-  INFO("assert that empty() returns false");
-  REQUIRE(dllist.empty() == false);
-  INFO("dllist: ");
-  dllist.print_list();
+    SUBCASE("test insert front") {
+        INFO("insert 5 numbers to the list at the front");
+        for (int i = 0; i < 5; i++) {
+            dllist.insert_front(i);
+        }
+        INFO("assert that the size = 5");
+        REQUIRE(dllist.get_size() == 5);
+        INFO("assert that empty() returns false");
+        REQUIRE(dllist.empty() == false);
+        INFO("dllist: ");
+        dllist.print_list();
+    }
 
-  INFO("delete from the front");
-  for (int i = 0; i < 5; i++) {
-    dllist.delete_front();
-    INFO("dllist after deleting front: ");
-    dllist.print_list();
-  }
-  INFO("assert that the size = 0");
-  REQUIRE(dllist.get_size() == 0);
-  INFO("assert that empty() returns true");
-  REQUIRE(dllist.empty() == true);
-  INFO("dllist: ");
-  dllist.print_list();
+    SUBCASE("test delete front") {
+        INFO("delete from the front");
+        for (int i = 0; i < 5; i++) {
+            int val = dllist.delete_front();
+            INFO("deleted ", val);
+        }
+        INFO("assert that the size = 0");
+        REQUIRE(dllist.get_size() == 0);
+        INFO("assert that empty() returns true");
+        REQUIRE(dllist.empty() == true);
+        INFO("dllist: ");
+        dllist.print_list();
+    }
 }
 
 TEST_CASE("insert back and delete back test") {
-  dllist<int> dllist;
-  // INFO("insert 5 numbers to the list at the back");
-  // INFO("assert that the size = 5");
-  // INFO("assert that empty() returns false");
-  // INFO("dllist: ");
+    dllist<int> dllist;
+    // INFO("insert 5 numbers to the list at the back");
+    // INFO("assert that the size = 5");
+    // INFO("assert that empty() returns false");
+    // INFO("dllist: ");
 }
 
 TEST_CASE("insert item and delete item test") {
-  dllist<int> dllist;
-  // insert at the front
+    dllist<int> dllist;
+    // insert at the front
 }
 
-TEST_CASE("modify test") { dllist<int> dllist; }
+TEST_CASE("modify test") {
+    dllist<int> dllist;
+}
 
 TEST_CASE("Destructor test") {
-  dllist<int> dllist;
-  INFO("initiliaze a new dllist");
-  // assert that the head is nullptr
-  // assert that the size = 0
-  // assert that empty() returns true
+    dllist<int> dllist;
+    INFO("initiliaze a new dllist");
+    // assert that the head is nullptr
+    // assert that the size = 0
+    // assert that empty() returns true
 }
