435e3595fc40bc4e0fa4cbe7ef9fce7ef1b452ff
Salvely <iamgwen9@gmail.com>
2024-09-13 16:14:01 +0800

    change INFO to MESSAGE

--- test/source/dllist_test.cpp
+++ test/source/dllist_test.cpp
@@ -5,7 +5,7 @@
 
 TEST_CASE("Initialization test") {
   dllist<int> dllist;
-  INFO("initiliaze a new dllist");
+  MESSAGE("initiliaze a new dllist");
   // assert that the head is nullptr
   REQUIRE(dllist.get_head() == nullptr);
   // assert that the size = 0
@@ -13,45 +13,47 @@ TEST_CASE("Initialization test") {
   // assert that empty() returns true
   REQUIRE(dllist.empty() == true);
   // print list
-  INFO("The dllist: ");
+  MESSAGE("The dllist: ");
   dllist.print_list();
 }
 
 TEST_SUITE("insert front and delete front test") {
   dllist<int> dllist;
 
-  INFO("insert 5 numbers to the list at the front");
+  MESSAGE("insert 5 numbers to the list at the front");
   for (int i = 0; i < 5; i++) {
     dllist.insert_front(i);
   }
-  INFO("assert that the size = 5");
+  MESSAGE("assert that the size = 5");
   REQUIRE(dllist.get_size() == 5);
-  INFO("assert that empty() returns false");
+  MESSAGE("assert that empty() returns false");
   REQUIRE(dllist.empty() == false);
 
-  INFO("dllist: ");
+  MESSAGE("dllist: ");
   dllist.print_list();
 
   REQUIRE(dllist.empty() == false);
-  INFO("delete from the front");
+  MESSAGE("delete from the front");
   for (int i = 0; i < 5; i++) {
     int val = dllist.delete_front();
-    INFO("deleted ", val);
+    MESSAGE("deleted ", val);
   }
-  INFO("assert that the size = 0");
+  MESSAGE("assert that the size = 0");
   REQUIRE(dllist.get_size() == 0);
-  INFO("assert that empty() returns true");
+  MESSAGE("assert that empty() returns true");
   REQUIRE(dllist.empty() == true);
-  INFO("dllist: ");
+  MESSAGE("dllist: ");
   dllist.print_list();
 }
 
+TEST_CASE("modify test") { dllist<int> dllist; }
+
 TEST_CASE("insert back and delete back test") {
   dllist<int> dllist;
-  // INFO("insert 5 numbers to the list at the back");
-  // INFO("assert that the size = 5");
-  // INFO("assert that empty() returns false");
-  // INFO("dllist: ");
+  // MESSAGE("insert 5 numbers to the list at the back");
+  // MESSAGE("assert that the size = 5");
+  // MESSAGE("assert that empty() returns false");
+  // MESSAGE("dllist: ");
 }
 
 TEST_CASE("insert item and delete item test") {
@@ -59,11 +61,9 @@ TEST_CASE("insert item and delete item test") {
   // insert at the front
 }
 
-TEST_CASE("modify test") { dllist<int> dllist; }
-
 TEST_CASE("Destructor test") {
   dllist<int> dllist;
-  INFO("initiliaze a new dllist");
+  MESSAGE("initiliaze a new dllist");
   // assert that the head is nullptr
   // assert that the size = 0
   // assert that empty() returns true
